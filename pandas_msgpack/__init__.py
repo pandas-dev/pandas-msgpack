7c5ea85289ec91ad790aaff116bc2d052ebfacf9
Jeff Reback <jeff@reback.net>
2017-03-30 13:43:30 -0400

    more fixing of legacy version

--- pandas_msgpack/__init__.py
+++ pandas_msgpack/__init__.py
@@ -8,7 +8,7 @@ pv = LooseVersion(pandas.__version__)
 
 if pv < '0.19.0':
     raise ValueError("pandas_msgpack requires at least pandas 0.19.0")
-_is_pandas_legacy_version = pv.version[1] == '19' and len(pv.version) == 3
+_is_pandas_legacy_version = pv.version[1] == 19 and len(pv.version) == 3
 
 from .packers import to_msgpack, read_msgpack
 
