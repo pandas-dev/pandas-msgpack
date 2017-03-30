754b366cddb76e1df4397c64e3c5e6751215f533
Jeff Reback <jeff@reback.net>
2017-03-30 11:39:17 -0400

    update installs

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -21,20 +21,22 @@ conda update -q conda
 
 conda info -a || exit 1
 
-conda create -n test-environment python=$PYTHON
+conda create -n test-environment python=$PYTHON cython pytest
 source activate test-environment
 
+pip install coverage pytest-cov flake8 codecov
 if [ $PANDAS == 'master' ]; then
+
+   conda install numpy pytz python-dateutil
 
    # install wheels from master
    PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
-   pip install --pre --upgrade --timeout=60 -f $PRE_WHEELS pandas numpy
+   pip install --pre --upgrade --timeout=60 -f $PRE_WHEELS pandas
 
 else
     conda install pandas=$PANDAS
 fi
 
-pip install coverage pytest pytest-cov flake8 codecov
 REQ="ci/requirements-${PYTHON}.pip"
 if [ -e $REQ ]; then
     pip install -r $REQ;
